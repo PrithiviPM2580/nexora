2f63c73c5b2710548b7c6624a21de35fdd5817d2
Prithivi <prithivi@users.noreply.github.com>
2026-04-15 23:07:21 +0545

    feat: add MainSection component for dashboard layout

---
+++ app/(routes)/(dashboard)/home/_common/main-section.tsx
@@ -0,0 +1,28 @@
+import { RiEmotionHappyFill } from "@remixicon/react"
+import React from "react"
+import Header from "../../_common/header"
+
+function MainSection() {
+  return (
+    <>
+      <Header />
+      <div className="relative w-full">
+        <div className="mx-auto w-full max-w-2xl space-y-5">
+          <div className="mt-16 flex w-full items-center justify-center">
+            <h1 className="fade-in-up z-0 flex items-center gap-2 text-center text-[24px] font-semibold tracking-tighter text-pretty text-gray-800 opacity-0 [animation-delay:200ms] sm:text-[30px] md:text-[35px] dark:text-white">
+              <RiEmotionHappyFill className="size-6" />
+              How can I help you today?
+            </h1>
+          </div>
+          <div className="w-full pt-7">
+            <div className="w-full">
+              <span className="text-sm dark:text-white/50">Recent notes</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default MainSection
