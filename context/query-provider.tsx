25262381d048a244a5380f23a9fc700c7acd9e09
Prithivi <prithivi@users.noreply.github.com>
2026-04-15 18:00:41 +0545

    feat: update QueryProvider to use React state for QueryClient initialization

--- context/query-provider.tsx
+++ context/query-provider.tsx
@@ -1,8 +1,10 @@
+"use client"
+
 import React from "react"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
 function QueryProvider({ children }: Readonly<{ children: React.ReactNode }>) {
-  const queryClient = new QueryClient()
+  const [queryClient] = React.useState(() => new QueryClient())
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   )
