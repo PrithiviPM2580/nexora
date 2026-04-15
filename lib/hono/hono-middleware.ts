ebbd523abaa65e87339cab37ef385e7125fd8bbb
Prithivi <prithivi@users.noreply.github.com>
2026-04-15 23:54:00 +0545

    feat: add getAuthUser middleware for session authentication

---
+++ lib/hono/hono-middleware.ts
@@ -0,0 +1,33 @@
+import { createMiddleware } from "hono/factory"
+import { auth } from "../auth"
+import { HTTPException } from "hono/http-exception"
+
+type AuthSession = Awaited<ReturnType<typeof auth.api.getSession>>
+type AuthUser = NonNullable<AuthSession>["user"]
+
+type Env = {
+  Variables: {
+    user: AuthUser
+  }
+}
+
+export const getAuthUser = createMiddleware<Env>(async (c, next) => {
+  try {
+    const session = await auth.api.getSession({
+      headers: c.req.raw.headers,
+    })
+
+    if (!session) {
+      throw new HTTPException(401, {
+        message: "Unauthorized",
+      })
+    }
+    c.set("user", session.user)
+    await next()
+  } catch (error) {
+    console.log("Error in getAuthUser middleware:", error)
+    throw new HTTPException(500, {
+      message: " Internal Server Error",
+    })
+  }
+})
