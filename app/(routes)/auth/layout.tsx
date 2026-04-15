87d110874c7d4288573090fc812a0ebefba1f999
Prithivi <prithivi@users.noreply.github.com>
2026-04-15 14:21:12 +0545

    feat: implement authentication session check and redirect in AuthLayout component

--- app/(routes)/auth/layout.tsx
+++ app/(routes)/auth/layout.tsx
@@ -1,6 +1,17 @@
 import React from "react"
+import { headers } from "next/headers"
+import { auth } from "@/lib/auth"
+import { redirect } from "next/navigation"
 
-function AuthLayout({ children }: { children: React.ReactNode }) {
+async function AuthLayout({ children }: { children: React.ReactNode }) {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  })
+
+  if (session) {
+    return redirect("/home")
+  }
+
   return <div>{children}</div>
 }
 
