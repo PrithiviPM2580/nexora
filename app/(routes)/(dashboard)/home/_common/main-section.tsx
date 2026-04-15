86e0ab4895a0a6cf0081aa6891762d5860ec68f8
Prithivi <prithivi@users.noreply.github.com>
2026-04-15 23:54:06 +0545

    feat: update RiEmotionHappyFill size class for responsive design

--- app/(routes)/(dashboard)/home/_common/main-section.tsx
+++ app/(routes)/(dashboard)/home/_common/main-section.tsx
@@ -1,5 +1,4 @@
 import { RiEmotionHappyFill } from "@remixicon/react"
-import React from "react"
 import Header from "../../_common/header"
 
 function MainSection() {
@@ -10,7 +9,7 @@ function MainSection() {
         <div className="mx-auto w-full max-w-2xl space-y-5">
           <div className="mt-16 flex w-full items-center justify-center">
             <h1 className="fade-in-up z-0 flex items-center gap-2 text-center text-[24px] font-semibold tracking-tighter text-pretty text-gray-800 opacity-0 [animation-delay:200ms] sm:text-[30px] md:text-[35px] dark:text-white">
-              <RiEmotionHappyFill className="size-6" />
+              <RiEmotionHappyFill className="size-6 md:size-10 lg:mt-2" />
               How can I help you today?
             </h1>
           </div>
