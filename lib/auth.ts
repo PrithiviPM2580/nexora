52fb9715f0595498efa5462111ec917fb6afc4e3
Prithivi <prithivi@users.noreply.github.com>
2026-04-15 14:21:40 +0545

    feat: implement authentication setup with better-auth and PrismaNeon adapter

---
+++ lib/auth.ts
@@ -0,0 +1,15 @@
+import { betterAuth } from "better-auth"
+import { prismaAdapter } from "better-auth/adapters/prisma"
+import { PrismaClient } from "@/generated/prisma/client"
+import { PrismaNeon } from "@prisma/adapter-neon"
+
+const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL })
+const prisma = new PrismaClient({ adapter })
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+})
