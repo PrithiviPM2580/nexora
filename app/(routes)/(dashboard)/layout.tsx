c285b55accab4c5ad63567ac52f7eb4e2bba5aef
Prithivi <prithivi@users.noreply.github.com>
2026-04-15 14:21:07 +0545

    feat: add session validation and redirect in DashboardLayout component

--- app/(routes)/(dashboard)/layout.tsx
+++ app/(routes)/(dashboard)/layout.tsx
@@ -1,11 +1,19 @@
 import React, { Suspense } from "react"
+import { headers } from "next/headers"
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
 import { NuqsAdapter } from "nuqs/adapters/next/app"
-import { RiLoader5Fill } from "@remixicon/react"
 import FallbackSpinner from "@/components/fallback-spinner"
 import AppSidebar from "@/components/sidebar"
+import { auth } from "@/lib/auth"
+import { redirect } from "next/navigation"
 
-function DashboardLayout({ children }: { children: React.ReactNode }) {
+async function DashboardLayout({ children }: { children: React.ReactNode }) {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  })
+  if (!session) {
+    return redirect("/auth/sign-in")
+  }
   return (
     <Suspense fallback={<FallbackSpinner />}>
       <NuqsAdapter>
