b67586fa51e5642fe74c1ced3792812ebfa73421
Prithivi <prithivi@users.noreply.github.com>
2026-04-15 14:21:17 +0545

    feat: add session management and redirect in WebLayout component

--- app/(routes)/(web)/layout.tsx
+++ app/(routes)/(web)/layout.tsx
@@ -1,6 +1,16 @@
 import React from "react"
+import { headers } from "next/headers"
+import { auth } from "@/lib/auth"
+import { redirect } from "next/navigation"
 
-function WebLayout({ children }: { children: React.ReactNode }) {
+async function WebLayout({ children }: { children: React.ReactNode }) {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  })
+
+  if (!session) {
+    return redirect("/home")
+  }
   return <div>WebLayout</div>
 }
 
