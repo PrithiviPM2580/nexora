a3f6848e2d95c62d1cb113f56abade333b1b1589
Prithivi <prithivi@users.noreply.github.com>
2026-04-16 12:55:48 +0545

    feat: enhance NavNotes component with loading state and note display

--- components/sidebar/nav-notes.tsx
+++ components/sidebar/nav-notes.tsx
@@ -1,29 +1,68 @@
 import React from "react"
 import {
-  SidebarContent,
   SidebarGroup,
   SidebarGroupAction,
   SidebarGroupContent,
   SidebarGroupLabel,
   SidebarMenu,
+  SidebarMenuButton,
   SidebarMenuItem,
 } from "../ui/sidebar"
-import { RiAddLine } from "@remixicon/react"
+import { RiAddLine, RiFileTextLine, RiLoader5Fill } from "@remixicon/react"
+import { useCreateNote, useNotes } from "@/features/use-note"
+import LoaderOverlay from "../loader-overlay"
 
 function NavNotes() {
+  const { data, isPending } = useNotes()
+  const { mutate, isPending: isLoading } = useCreateNote()
+
+  const notes = data?.data ?? []
+  const onCreate = () => {
+    mutate({
+      title: "United",
+      content: "",
+    })
+  }
   return (
     <>
+      <LoaderOverlay show={isLoading} />
       <SidebarGroup>
         <SidebarGroupLabel>
           <h5>Notes</h5>
-          <SidebarGroupAction className="mt-[1.5px] flex size-5.5 cursor-pointer items-center rounded-md border bg-primary/20">
+          <SidebarGroupAction
+            className="mt-[1.5px] flex size-5.5 cursor-pointer items-center rounded-md border bg-primary/20"
+            onClick={onCreate}
+          >
             <RiAddLine className="size-5" />
             <span className="sr-only">Add Notes</span>
           </SidebarGroupAction>
         </SidebarGroupLabel>
-        <SidebarGroupContent className="h-auto max-h-90 min-h-32 w-full overflow-y-hidden">
+        <SidebarGroupContent className="mt-2 h-auto max-h-90 min-h-32 w-full overflow-y-hidden">
           <SidebarMenu>
-            <SidebarMenuItem>No Notes</SidebarMenuItem>
+            {notes.length === 0 ? (
+              <div className="">No Notes</div>
+            ) : isPending ? (
+              <div className="flex items-center justify-center">
+                <RiLoader5Fill className="h-8 w-8 animate-spin text-primary" />
+              </div>
+            ) : (
+              notes?.map((note) => {
+                // const isActive = note.id === noteId
+                return (
+                  <SidebarMenuItem key={note.id}>
+                    <SidebarMenuButton
+                      className="flex w-full items-center"
+                      onClick={() => {}}
+                    >
+                      <span className="flex h-8 w-8 items-center justify-center rounded-lg bg-secondary">
+                        <RiFileTextLine className="h-4 w-4 text-primary" />
+                      </span>
+                      <h5 className="flex-1 truncate">{note.title}</h5>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                )
+              })
+            )}
           </SidebarMenu>
         </SidebarGroupContent>
       </SidebarGroup>
