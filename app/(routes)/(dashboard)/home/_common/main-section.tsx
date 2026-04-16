ceef5138a612be78a2833c94f2166af0e1851f13
Prithivi <prithivi@users.noreply.github.com>
2026-04-16 15:47:51 +0545

    feat: add RecentNotes component to MainSection for displaying recent notes

--- app/(routes)/(dashboard)/home/_common/main-section.tsx
+++ app/(routes)/(dashboard)/home/_common/main-section.tsx
@@ -1,11 +1,12 @@
 import { RiEmotionHappyFill } from "@remixicon/react"
 import Header from "../../_common/header"
+import RecentNotes from "./recent-notes"
 
 function MainSection() {
   return (
     <>
       <Header />
-      <div className="relative w-full">
+      <div className="relative w-full px-2">
         <div className="mx-auto w-full max-w-2xl space-y-5">
           <div className="mt-16 flex w-full items-center justify-center">
             <h1 className="fade-in-up z-0 flex items-center gap-2 text-center text-[24px] font-semibold tracking-tighter text-pretty text-gray-800 opacity-0 [animation-delay:200ms] sm:text-[30px] md:text-[35px] dark:text-white">
@@ -17,6 +18,7 @@ function MainSection() {
             <div className="w-full">
               <span className="text-sm dark:text-white/50">Recent notes</span>
             </div>
+            <RecentNotes />
           </div>
         </div>
       </div>
